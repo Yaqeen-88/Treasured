1cb61b9442206d6396369d116ce98ba52f45b8a9
Your Username <youremail@domain.com>
2025-11-09 18:52:38 +0300

    add user model

--- server.js
+++ server.js
@@ -10,29 +10,25 @@ const morgan = require("morgan");
 
 
 const postRouter = require("./routes/posts"); 
+const { connected } = require("process");
 
 
 const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGODB_URI)
 
-mongoose.connection.on("connected", () => {
-  console.log(`Connected to MongoDB: ${mongoose.connection.name}`);
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error("MongoDB connection error:", err);
-});
-
+mongoose.connection.on('connected', () => {
+  console.log(`connected to mongodb ${mongoose.connection.name}.`)
+})
 
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride("_method")); 
 app.use(morgan("dev")); 
 
-app.set("view engine", "ejs");
+
+app.get('/', async (req,res) => {
+  res.render('index.ejs')
+})
 
 app.get("/", (req, res) => {
   res.redirect("/posts"); 
