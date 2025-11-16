049c9b61dbe095a5f0d512f6e9003ae64994cdad
Danalajmi <danaralajami@gmail.com>
2025-11-16 20:37:27 +0300

    idk

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,5 +1,6 @@
 const Post = require("../models/post")
 const User = require('../models/user')
+
 exports.profile_index_get = async (req,res) => {
   const myPosts = await Post.find({creator: req.params.userID}).populate('creator')
   const postCount = myPosts.length
@@ -7,7 +8,12 @@ exports.profile_index_get = async (req,res) => {
 }
 
 exports.profile_edit_get = async (req,res) => {
-  const account = await User.findById(req.session.user._id)
+  const account = await User.findById(req.params.userID)
   console.log(account)
-  res.render('profile/edit.ejs', account)
+  res.render('profile/edit.ejs', {account})
+}
+
+exports.profile_edit_post = async (req,res) => {
+  const account = await User.findById(req.params.userID)
+
 }
--- routes/profiles.js
+++ routes/profiles.js
@@ -3,6 +3,6 @@ const profileCTRL = require('../controllers/profile')
 const router = require("express").Router()
 
 router.get('/:userID', profileCTRL.profile_index_get)
-router.get('/', profileCTRL.profile_edit_get)
+router.get('/:userID/edit', profileCTRL.profile_edit_get)
 
 module.exports = router
